11f8bfb1e7ff691843a0d1afc5f05095db6747e1
velinovasen <velinovasen@users.noreply.github.com>
2020-09-17 00:03:15 +0300

    'dsd'

--- predictions.py
+++ predictions.py
@@ -3,3 +3,10 @@ from selenium.webdriver import Chrome, ChromeOptions
 import bs4
 import re
 from time import sleep
+
+
+class Predictions:
+
+    WEB_LINKS = {
+        "football": 'https://www.forebet.com/en/top-football-tips-and-predictions'
+    }
