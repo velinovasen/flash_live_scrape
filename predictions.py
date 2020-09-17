4c47b59312894aebe2b1fbc155ccdaa3ff5f1ce0
velinovasen <velinovasen@users.noreply.github.com>
2020-09-17 11:21:53 +0300

    'got_the_teams'

--- predictions.py
+++ predictions.py
@@ -8,7 +8,8 @@ from time import sleep
 class Predictions:
 
     WEB_LINKS = {
-        'football': 'https://www.forebet.com/en/top-football-tips-and-predictions'
+        'football_today': 'https://m.forebet.com/en/football-tips-and-predictions-for-today',
+        'football_tomorrow': 'https://m.forebet.com/en/football-tips-and-predictions-for-tomorrow'
     }
 
     def scrape(self):
@@ -22,12 +23,30 @@ class Predictions:
 
         # OPEN THE WEBSITE AND GET THE DATA
         options = ChromeOptions()
-        options.headless = True
+        options.headless = False   # IF YOU WANT TO SEE THE BROWSER -> FALSE
         driver = Chrome(options=options, executable_path='C://Windows/chromedriver.exe')
-        driver.get(self.WEB_LINKS['football'])
+        driver_tomorrow = Chrome(options=options, executable_path='C://Windows/chromedriver.exe')
+        driver.get(self.WEB_LINKS['football_today'])
+        driver_tomorrow.get(self.WEB_LINKS['football_tomorrow'])
         sleep(2)
-        html = driver.execute_script('return document.documentElement.outerHTML;')
+        # TO DO - > PRESS [MORE] BUTTON ON THE BOTTOM UNTIL DISAPPEAR
+        html_today = driver.execute_script('return document.documentElement.outerHTML;')
+        html_tomorrow = driver_tomorrow.execute_script('return document.documentElement.outerHTML;')
 
-
+        # WORK WITH THE DATA
+        today_soup = bs4.BeautifulSoup(html_today, 'html.parser')
+        tomorrow_soup = bs4.BeautifulSoup(html_tomorrow, 'html.parser')
+        matches_one_today = today_soup.find_all(class_=re.compile('tr_0'))
+        matches_two_today = today_soup.find_all(class_=re.compile('tr_1'))
+        matches_one_tomorrow = tomorrow_soup.find_all(class_=re.compile('tr_0'))
+        matches_two_tomorrow = tomorrow_soup.find_all(class_=re.compile('tr_1'))
+        [print(game) for game in matches_one_today]
+        [print(game) for game in matches_two_today]
+        [print(game) for game in matches_one_tomorrow]
+        [print(game) for game in matches_two_tomorrow]
         driver.close()
         cursor.close()
+
+
+scraper = Predictions()
+scraper.scrape()
