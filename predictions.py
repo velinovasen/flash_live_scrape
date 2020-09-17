0c906006a1cf8fece6234101caa54186f7217704
velinovasen <velinovasen@users.noreply.github.com>
2020-09-17 09:09:05 +0300

    'ds'd

--- predictions.py
+++ predictions.py
@@ -10,3 +10,12 @@ class Predictions:
     WEB_LINKS = {
         "football": 'https://www.forebet.com/en/top-football-tips-and-predictions'
     }
+
+    def scrape(self):
+        # TO DO - CONNECT THE DATABASE
+        connector = sqlite3.connect('games-db')
+        cursor = connector.cursor()
+        cursor.execute("DROP TABLE IF EXISTS Predictions")
+        cursor.execute('CREATE TABLE Predictions(time TEXT, home_team TEXT, away_team TEXT,'
+                       ' home_prob DECIMAL, draw_prob DECIMAL, away_prob DECIMAL, bet_sign DECIMAL,'
+                       ' score_predict TEXT, avg_goals REAL, odds REAL, temp TEXT)')
